06a8bcb057b1b04332d55d516d6da5638e0745b5
Victor <paulo9.victor.silva@gmail.com>
2024-05-21 00:55:02 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from './swagger.json';
+
+const app = express();
+const port = 8000;
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.redirect('/api-docs');
+});
+
+app.listen(port, () => { console.log(`listening on ${port}`) });
